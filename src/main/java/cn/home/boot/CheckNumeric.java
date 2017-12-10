7c20b05eed579c4f8a9c8f1c7d80501a98c7e9d5
doanldhan <shaoqinghanah@qq.com>
2017-12-10 21:57:59 +0800

    init project

---
+++ src/main/java/cn/home/boot/CheckNumeric.java
@@ -0,0 +1,23 @@
+package cn.home.boot;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author donald
+ * 2017年12月10日
+ * 下午9:38:35
+ */
+public class CheckNumeric {
+	private static final Logger log = LoggerFactory.getLogger(CheckNumeric.class);
+
+	public static void main(String[] args) {
+		String text1 = "a12340";
+		String text2 = "1234";
+		boolean result1 = StringUtils.isNumeric(text1);
+		boolean result2 = StringUtils.isNumeric(text2);
+		log.info("{} is a numeric: {}",text1,result1);
+		log.info("{} is a numeric: {}",text2,result2);
+	}
+}
---
+++ src/main/java/cn/home/boot/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author donald
+ * 2017年12月10日
+ * 下午9:10:41
+ */
+package cn.home.boot;
